cf4ae1d3d648399660b1a428b13ddeeb01e48c34
danielkropf <danielkropf@uol.com.br>
2016-06-07 16:03:52 -0300

    Version 0.1

---
+++ Controle/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Controle
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tab1YearController = new System.Windows.Forms.NumericUpDown();
+			this.tab1MonthController = new System.Windows.Forms.NumericUpDown();
+			this.tab1DayController = new System.Windows.Forms.NumericUpDown();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.directoryEntry1 = new System.DirectoryServices.DirectoryEntry();
+			this.menuStrip1.SuspendLayout();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.tab1YearController)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.tab1MonthController)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.tab1DayController)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(629, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// toolStripMenuItem1
+			// 
+			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(22, 20);
+			this.toolStripMenuItem1.Text = ".";
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Location = new System.Drawing.Point(0, 27);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(629, 278);
+			this.tabControl1.TabIndex = 1;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.numericUpDown1);
+			this.tabPage1.Controls.Add(this.tab1YearController);
+			this.tabPage1.Controls.Add(this.tab1MonthController);
+			this.tabPage1.Controls.Add(this.tab1DayController);
+			this.tabPage1.Controls.Add(this.label4);
+			this.tabPage1.Controls.Add(this.label3);
+			this.tabPage1.Controls.Add(this.label2);
+			this.tabPage1.Controls.Add(this.label1);
+			this.tabPage1.Controls.Add(this.comboBox2);
+			this.tabPage1.Controls.Add(this.comboBox1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(621, 252);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "tabPage1";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tab1YearController
+			// 
+			this.tab1YearController.Location = new System.Drawing.Point(523, 116);
+			this.tab1YearController.Name = "tab1YearController";
+			this.tab1YearController.ReadOnly = true;
+			this.tab1YearController.Size = new System.Drawing.Size(53, 20);
+			this.tab1YearController.TabIndex = 12;
+			// 
+			// tab1MonthController
+			// 
+			this.tab1MonthController.Location = new System.Drawing.Point(481, 116);
+			this.tab1MonthController.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+			this.tab1MonthController.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.tab1MonthController.Name = "tab1MonthController";
+			this.tab1MonthController.ReadOnly = true;
+			this.tab1MonthController.Size = new System.Drawing.Size(36, 20);
+			this.tab1MonthController.TabIndex = 11;
+			this.tab1MonthController.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.tab1MonthController.ValueChanged += new System.EventHandler(this.ChangeMonth);
+			// 
+			// tab1DayController
+			// 
+			this.tab1DayController.Location = new System.Drawing.Point(439, 116);
+			this.tab1DayController.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+			this.tab1DayController.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.tab1DayController.Name = "tab1DayController";
+			this.tab1DayController.ReadOnly = true;
+			this.tab1DayController.Size = new System.Drawing.Size(36, 20);
+			this.tab1DayController.TabIndex = 10;
+			this.tab1DayController.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(436, 98);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(30, 13);
+			this.label4.TabIndex = 9;
+			this.label4.Text = "Data";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(309, 98);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(62, 13);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "Quantidade";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(182, 98);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(44, 13);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "Produto";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(55, 99);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(62, 13);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "Funcionario";
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Location = new System.Drawing.Point(185, 115);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(121, 21);
+			this.comboBox2.TabIndex = 3;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(58, 115);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 2;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(621, 252);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "tabPage2";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(312, 114);
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.ReadOnly = true;
+			this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+			this.numericUpDown1.TabIndex = 13;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			// 
+			// directoryEntry1
+			// 
+			this.directoryEntry1.Path = "C:\\Users\\danie\\Desktop\\Arquivos\\Visual Studio\\Controle\\Controle";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(629, 305);
+			this.Controls.Add(this.tabControl1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.tab1YearController)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.tab1MonthController)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.tab1DayController)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.NumericUpDown tab1YearController;
+		private System.Windows.Forms.NumericUpDown tab1MonthController;
+		private System.Windows.Forms.NumericUpDown tab1DayController;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.DirectoryServices.DirectoryEntry directoryEntry1;
+	}
+}
+
---
+++ Controle/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace Controle
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void ChangeMonth(object sender, EventArgs e)
+		{
+			if(tab1MonthController.Value == 1 || tab1MonthController.Value == 3 || tab1MonthController.Value == 5 || tab1MonthController.Value == 7 || tab1MonthController.Value == 8 || tab1MonthController.Value == 10 || tab1MonthController.Value == 12)
+			{
+				tab1DayController.Maximum = 31;
+			}
+			else if (tab1MonthController.Value == 4 || tab1MonthController.Value == 6 || tab1MonthController.Value == 9 || tab1MonthController.Value == 11)
+			{
+				tab1DayController.Maximum = 30;
+			}
+			else if (tab1MonthController.Value == 2)
+			{
+				if(DateTime.Now.Year % 4 == 0)
+				{
+					tab1DayController.Maximum = 29;
+				}
+				else
+				{
+					tab1DayController.Maximum = 28;
+				}
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			tab1YearController.Minimum = DateTime.Now.Year - 2;
+			tab1YearController.Maximum = DateTime.Now.Year + 5;
+			tab1YearController.Value = DateTime.Now.Year;
+
+		}
+	}
+}
